cb61287a88e9b907d8b62577a678df90f5a96caa
hasanyiilmaz1 <112687161+hasanyiilmaz1@users.noreply.github.com>
2022-09-07 16:39:28 +0300

    Update timer.cy.js

--- react-timer/cypress/e2e/timer.cy.js
+++ react-timer/cypress/e2e/timer.cy.js
@@ -4,19 +4,26 @@ describe('Start and Stop', () => {
 
   })
   it('start run',async () => {
+    
     //start butona basıldı
     cy.contains('Start').click() 
+    cy.screenshot('Start')
     // 10sn ye ayarlandı sn geri saydı
     cy.wait(10000)
+    cy.get('[data-testid="gosterge"]').should('have.text', '24:50')
     //Sayım duraklatıldı
     cy.contains('Stop').click()
+    cy.screenshot('Stop')
     cy.wait(5000)
     //Durdurduktan sonra sayıma devam edildi
     cy.contains('Start').click() 
-    //Reset butonuna basıldı 
+    cy.screenshot('Start')
+       //Reset butonuna basıldı 
     cy.wait(10000)
+    cy.get('[data-testid="gosterge"]').should('have.text', '24:40')
     cy.contains('Reset').click()
- 
+    cy.screenshot('Reset')
+    cy.get('[data-testid="gosterge"]').should('have.text', '25:00')
    
   })
   
